b3eda3a5a8e44d806dacc88ff947b2ff1da90746
Sahil Kashetwar <sahil@enya.ai>
2023-10-01 16:38:45 +0530

    Integration/E2E test setup:
    - Setting up cypress / sypress plugins / env configuration / package script
    - Added helper functions to prepare test cases
    - Added layout test with header validation in case of ethereum network

---
+++ cypress/e2e/helpers/base/page.ts
@@ -0,0 +1,305 @@
+/// <reference types="cypress"/>
+import Base from './base'
+import PageHeader from './page.header'
+import PageFooter from './page.footer'
+
+export default class Page extends Base {
+  header: PageHeader
+  footer: PageFooter
+  walletConnectButtonText: string
+  id: string
+  constructor() {
+    super()
+    this.header = new PageHeader()
+    this.footer = new PageFooter()
+    this.id = 'header'
+    this.walletConnectButtonText = 'Connect Wallet'
+  }
+
+  visit() {
+    cy.visit(`/${this.id}`)
+  }
+  withinPage() {
+    return cy.get(`#${this.id}`)
+  }
+  getReduxStore() {
+    return cy.window().its('store').invoke('getState')
+  }
+  connectWallet() {
+    this.withinPage()
+      .contains('button', this.walletConnectButtonText)
+      .should('exist')
+      .click()
+  }
+  requestMetamaskConnect() {
+    this.connectWallet()
+    cy.get('#connectMetaMask').should('exist').click()
+  }
+
+  checkNaviagtionListBinanace() {
+    this.header
+      .getNavigationLinks()
+      .should('not.be.empty')
+      .and(($p) => {
+        // should have found 4 elements for Binanace
+        expect($p).to.have.length(4)
+
+        // // use jquery's map to grab all of their classes
+        // // jquery's map returns a new jquery object
+        const links = $p.map((i, el) => {
+          return Cypress.$(el).attr('href')
+        })
+        // call classes.get() to make this a plain array
+        expect(links.get()).to.deep.eq([
+          '/bridge',
+          '/bridge',
+          '/history',
+          '/earn',
+        ])
+
+        // get labels and verify
+        const labels = $p.map((i, el) => {
+          return Cypress.$(el).text()
+        })
+
+        expect(labels.get()).to.deep.eq(['', 'Bridge', 'History', 'Earn'])
+      })
+  }
+
+  checkNaviagtionListAvalanche() {
+    this.header
+      .getNavigationLinks()
+      .should('not.be.empty')
+      .and(($p) => {
+        // should have found 4 elements for Avalanche
+        expect($p).to.have.length(6)
+
+        // // use jquery's map to grab all of their classes
+        // // jquery's map returns a new jquery object
+        const links = $p.map((i, el) => {
+          return Cypress.$(el).attr('href')
+        })
+        // call classes.get() to make this a plain array
+        expect(links.get()).to.deep.eq([
+          '/bridge',
+          '/bridge',
+          '/history',
+          '/earn',
+        ])
+
+        // get labels and verify
+        const labels = $p.map((i, el) => {
+          return Cypress.$(el).text()
+        })
+
+        expect(labels.get()).to.deep.eq(['', 'Bridge', 'History', 'Earn'])
+      })
+  }
+
+  checkNavigationListEthereum() {
+    this.header
+      .getNavigationLinks()
+      .should('not.be.empty')
+      .and(($p) => {
+        // should have found 6 elements for Ethereum
+        expect($p).to.have.length(6)
+
+        // // use jquery's map to grab all of their classes
+        // // jquery's map returns a new jquery object
+        const links = $p.map((i, el) => {
+          return Cypress.$(el).attr('href')
+        })
+        // call classes.get() to make this a plain array
+        expect(links.get()).to.deep.eq([
+          '/bridge',
+          '/bridge',
+          '/history',
+          '/earn',
+          '/stake',
+          '/DAO',
+        ])
+
+        // get labels and verify
+        const labels = $p.map((i, el) => {
+          return Cypress.$(el).text()
+        })
+
+        expect(labels.get()).to.deep.eq([
+          '',
+          'Bridge',
+          'History',
+          'Earn',
+          'Stake',
+          'Dao',
+        ])
+      })
+  }
+  checkNetworkSwitcherMainnet() {
+    this.header.getNetworkSwitcher().click()
+
+    this.header.getNetworkSwitcher().contains('Ethereum').should('exist')
+
+    this.header
+      .getNetworkSwitcher()
+      .contains('Binance Smart Chain')
+      .should('exist')
+
+    this.header
+      .getNetworkSwitcher()
+      .contains('Avalanche Mainnet C-Chain')
+      .should('exist')
+
+    this.header.getNetworkSwitcher().click()
+  }
+  checkNetworkSwitcherTestnet() {
+    this.header
+      .getNetworkSwitcher()
+      .click()
+      .should('have.text', 'Ethereum (Goerli)')
+      .should('have.text', 'BNB Testnet')
+      .should('have.text', 'Fuji Testnet')
+  }
+  checkThemeSwitcher() {
+    this.header.getLightThemeSwitcher().should('exist').click()
+    this.header.getDarkThemeSwitcher().should('exist').click()
+    this.header.getLightThemeSwitcher().should('exist')
+  }
+
+  checkSocialMediaLinks() {
+    this.footer
+      .getSocialMediaLinks()
+      .should('not.be.empty')
+      .and(($p) => {
+        // should have found 4 elements
+        expect($p).to.have.length(4)
+
+        // // use jquery's map to grab all of their classes
+        // // jquery's map returns a new jquery object
+        const links = $p.map((i, el) => {
+          return Cypress.$(el).attr('href')
+        })
+        // call classes.get() to make this a plain array
+        expect(links.get()).to.deep.eq([
+          'https://docs.boba.network',
+          'https://boba.eco/twitter',
+          'https://boba.eco/discord',
+          'https://boba.eco/telegram',
+        ])
+
+        // get labels and verify
+        const labels = $p.map((i, el) => {
+          return Cypress.$(el).attr('aria-label')
+        })
+
+        expect(labels.get()).to.deep.eq([
+          'bobaDocs',
+          'twitter',
+          'discord',
+          'telegram',
+        ])
+      })
+  }
+  checkFooterLinks() {
+    this.footer
+      .getFooterLinks()
+      .should('not.be.empty')
+      .and(($p) => {
+        // should have found 4 elements
+        expect($p).to.have.length(7)
+        // make sure the first contains some text content
+        expect($p.first()).to.contain('FAQs')
+        // // use jquery's map to grab all of their classes
+        // // jquery's map returns a new jquery object
+        const links = $p.map((i, el) => {
+          return Cypress.$(el).attr('href')
+        })
+        // call classes.get() to make this a plain array
+        expect(links.get()).to.deep.eq([
+          'https://docs.boba.network/faq',
+          '/devtools',
+          '/bobascope',
+          'https://boba.network',
+          'https://boba.network/terms-of-use/',
+        ])
+        // get labels and verify
+        const labels = $p.map((i, el) => {
+          return Cypress.$(el).text()
+        })
+        expect(labels.get()).to.deep.eq([
+          'FAQs',
+          'Dev Tools',
+          'Bobascope',
+          'Boba Network Website',
+          'Terms of Use',
+        ])
+      })
+  }
+
+  handleNetworkSwitchModals(networkAbbreviation: string, isTestnet: boolean) {
+    cy.get(
+      `button[label="Switch to ${networkAbbreviation} ${
+        isTestnet ? 'Testnet' : ''
+      } network"]`,
+      { timeout: 90000 }
+    )
+      .should('exist')
+      .click()
+
+    this.verifyReduxStoreSetup('accountEnabled', false)
+    this.verifyReduxStoreSetup('baseEnabled', false)
+
+    cy.get(
+      `button[label="Connect to the ${networkAbbreviation} ${
+        isTestnet ? 'Testnet' : ''
+      } network"]`,
+      { timeout: 90000 }
+    )
+      .should('exist')
+      .click()
+  }
+
+  switchThroughMainnetNetworks() {
+    // switch to BNB
+    this.header.switchNetwork('BNB')
+    this.handleNetworkSwitchModals('BNB', false)
+    this.allowNetworkSwitch()
+    this.checkNetworkSwitchSuccessful('BNB')
+
+    // switch to AVAX
+    this.header.switchNetwork('Avalanche Mainnet')
+    this.handleNetworkSwitchModals('AVAX', false)
+    this.allowNetworkSwitch()
+    this.checkNetworkSwitchSuccessful('AVAX')
+
+    // switch to Ethereum
+    this.header.switchNetwork('Ethereum')
+    this.handleNetworkSwitchModals('ETHEREUM', false)
+    this.allowNetworkSwitch()
+    this.checkNetworkSwitchSuccessful('ETHEREUM')
+  }
+
+  checkNetworkSwitchSuccessful(networkAbbreviation: string) {
+    this.verifyReduxStoreNetwork('activeNetwork', networkAbbreviation)
+
+    this.verifyReduxStoreSetup('accountEnabled', true)
+    this.verifyReduxStoreSetup('baseEnabled', true)
+  }
+
+  verifyReduxStoreSetup(attribute: string, expectedValue: boolean | string) {
+    this.getReduxStore()
+      .its('setup')
+      .its(attribute)
+      .should(`equal`, expectedValue)
+  }
+
+  verifyReduxStoreNetwork(attribute: string, expectedValue: boolean | string) {
+    this.getReduxStore()
+      .its('network')
+      .its(attribute)
+      .should('equal', expectedValue)
+  }
+
+  disconnectWallet() {
+    this.header.disconnectWallet()
+  }
+}
---
+++ cypress/e2e/helpers/history.ts
@@ -0,0 +1,302 @@
+/// <reference types="cypress"/>
+import Page from './base/page'
+import dayjs from 'dayjs'
+import {
+  formatDate,
+  isSameOrAfterDate,
+  isSameOrBeforeDate,
+} from '../../../src/util/dates'
+import { Layer } from '../../../src/util/constant'
+import {
+  TRANSACTION_FILTER_STATUS,
+  CHAIN_NAME,
+} from '../../../src/containers/history/types'
+// import truncate from 'truncate-middle'
+
+export default class History extends Page {
+  fromDate: Date
+  toDate: Date
+  networkLayerFrom: Layer
+  fromNetwork: string
+  toNetwork: string
+  constructor() {
+    super()
+    this.toDate = new Date()
+    this.fromDate = new Date(
+      this.toDate.getFullYear(),
+      this.toDate.getMonth() - 6,
+      this.toDate.getDate()
+    )
+    this.id = 'history'
+    this.walletConnectButtonText = 'Connect Wallet'
+    this.networkLayerFrom = Layer.L1
+    this.fromNetwork = 'All Networks'
+    this.toNetwork = 'All Networks'
+  }
+  getSearchInput() {
+    return this.withinPage()
+      .get('input[placeholder="Search Here"]')
+      .should('exist')
+  }
+  getFromDatePicker() {
+    const dateString = formatDate(this.fromDate.getTime() / 1000, 'MM/DD/YYYY')
+    return this.withinPage().get('div').contains(dateString).should('exist')
+  }
+  getToDatePicker() {
+    const dateString = formatDate(this.toDate.getTime() / 1000, 'MM/DD/YYYY')
+    return this.withinPage().get('div').contains(dateString).should('exist')
+  }
+  getNetworkDropdowns() {
+    return this.withinPage().find('#networkDropdowns')
+  }
+
+  getFromNetworkDropdown() {
+    return this.getNetworkDropdowns().children().filter('div').first()
+  }
+  getToNetworkDropdown() {
+    return this.getNetworkDropdowns().children().filter('div').last()
+  }
+
+  async changeFromNetwork(networkName: string) {
+    this.getFromNetworkDropdown()
+      .contains(this.fromNetwork)
+      .should('exist')
+      .click()
+    this.getFromNetworkDropdown().contains(networkName).should('exist').click()
+    this.fromNetwork = networkName
+  }
+
+  changeToNetwork(networkName: string) {
+    this.getToNetworkDropdown().contains(this.toNetwork).should('exist').click()
+    this.getToNetworkDropdown().contains(networkName).should('exist').click()
+    this.toNetwork = networkName
+  }
+  verifyFilteredTransactionsByColumn(column: string, content: string) {
+    this.getTransactionsColumn(column).then((transactionTableColumn) => {
+      for (const transactionInfo of transactionTableColumn) {
+        cy.wrap(transactionInfo).contains(content).should('exist')
+      }
+    })
+  }
+
+  checkNetworkDropdowns() {
+    this.getNetworkDropdowns()
+      .find(':contains("All Networks")')
+      .not(':has(*)') // checks that elements are leafs in the HTML tree
+      .should('not.be.empty')
+      .and((networkDropdowns) => {
+        const labels = networkDropdowns.map((i, networkDropdown) => {
+          return Cypress.$(networkDropdown).text()
+        })
+        expect(labels.get()).to.deep.eq(['All Networks', 'All Networks'])
+      })
+
+    // ensure that network dropdowns are in the correct order by default
+    this.getFromNetworkDropdown()
+      .invoke('attr', 'id')
+      .should('equal', 'L1Networks')
+    this.getToNetworkDropdown()
+      .invoke('attr', 'id')
+      .should('equal', 'L2Networks')
+
+    // properly filters transactions by chosen 'from' network
+    this.changeFromNetwork(CHAIN_NAME.Goerli)
+    this.verifyFilteredTransactionsByColumn(
+      'transactionOriginDetails',
+      CHAIN_NAME.Goerli
+    )
+
+    this.changeFromNetwork('All Networks')
+
+    // properly filters transactions by chosen 'to' network
+    this.changeToNetwork(CHAIN_NAME.Boba_Goerli)
+    this.verifyFilteredTransactionsByColumn(
+      'transactionDestinationDetails',
+      CHAIN_NAME.Boba_Goerli
+    )
+
+    // test network switch
+    this.switchNetworkDropdowns()
+    this.verifyFilteredTransactionsByColumn(
+      'transactionOriginDetails',
+      CHAIN_NAME.Boba_Goerli
+    )
+
+    this.switchNetworkDropdowns()
+    this.changeToNetwork('All Networks')
+  }
+
+  switchNetworkDropdowns() {
+    this.getNetworksSwitchIcon().click()
+    const networkHolder = this.fromNetwork
+    this.fromNetwork = this.toNetwork
+    this.toNetwork = networkHolder
+  }
+
+  getNetworksSwitchIcon() {
+    return this.withinPage()
+      .find('#switchNetworkDirection')
+      .should('exist')
+      .find('svg')
+      .should('exist')
+  }
+
+  getStatusFilter() {
+    return this.withinPage().find('#statusFilter').should('exist')
+  }
+  getTransactions() {
+    return this.withinPage().get('#transactionList').children()
+  }
+
+  waitForTransactionsToLoad() {
+    cy.window()
+      .its('store')
+      .invoke('getState')
+      .its('transaction', { timeout: 90000 })
+      .should('not.be.empty')
+  }
+
+  checkSearchInput() {
+    const hash = Cypress.env('target_hash')
+    this.getSearchInput().focus().type(hash)
+    // this.getTransactions().contains(truncate(hash, 4, 4, '...')).should('exist')
+    this.getTransactions().should('have.length', 1)
+    this.getSearchInput().clear()
+  }
+
+  changeDatePicker(from: boolean, targetDate: Date, shouldBeDisabled = false) {
+    const originalDate = from ? this.fromDate : this.toDate
+    const datePicker = from ? this.getFromDatePicker() : this.getToDatePicker()
+
+    datePicker.click()
+    const monthDelta = targetDate.getMonth() - originalDate.getMonth()
+    if (monthDelta > 0) {
+      for (let i = 0; i < monthDelta; i++) {
+        this.withinPage().find('button[aria-label="Go to next month"]').click()
+      }
+    }
+    if (monthDelta < 0) {
+      for (let i = 0; i < monthDelta * -1; i++) {
+        this.withinPage()
+          .find('button[aria-label="Go to previous month"]')
+          .click()
+      }
+    }
+
+    if (shouldBeDisabled) {
+      this.withinPage()
+        .find('button[name=day]')
+        .contains(targetDate.getDate())
+        .invoke('attr', 'disabled')
+        .should('exist')
+    } else {
+      this.withinPage()
+        .find('button[name=day]')
+        .contains(targetDate.getDate())
+        .should('have.length', 1)
+        .click()
+      if (from) {
+        this.fromDate = targetDate
+      } else {
+        this.toDate = targetDate
+      }
+    }
+    datePicker.click()
+  }
+
+  verifyTransactionsByDate(fromDate: Date, toDate: Date) {
+    this.getTransactionsColumn('transactionDate').then(($date) => {
+      const transactionDates = $date.map((i, element) =>
+        Cypress.$(element).text()
+      )
+      for (const transactionDateStr of transactionDates.get()) {
+        const transactionTimestamp = dayjs(new Date(transactionDateStr)).unix()
+        expect(isSameOrAfterDate(transactionTimestamp, dayjs(fromDate))).to.be
+          .true
+        expect(isSameOrBeforeDate(transactionTimestamp, dayjs(toDate))).to.be
+          .true
+      }
+    })
+  }
+
+  checkDatePickers() {
+    const orignalFromDate = this.fromDate
+    const orignalToDate = this.toDate
+    this.changeDatePicker(true, new Date('30 Jun 2023 12:00 AM'))
+    this.changeDatePicker(false, new Date('30 Aug 2023 12:00 AM'))
+
+    // ensure transactions occured within the date specified in the date pickers
+    this.verifyTransactionsByDate(this.fromDate, this.toDate)
+
+    // dates after 'to date' should be disabled in 'from' date picker
+    this.changeDatePicker(true, new Date('31 Aug 2023 12:00 AM'), true)
+
+    // dates before 'from date' should be disabled in 'to' date picker
+    this.changeDatePicker(false, new Date('29 Jun 2023 12:00 AM'), true)
+
+    this.changeDatePicker(true, orignalFromDate)
+    this.changeDatePicker(false, orignalToDate)
+  }
+
+  changeStatusFilter(status: TRANSACTION_FILTER_STATUS) {
+    this.getStatusFilter().find('svg').should('exist').click()
+    this.getStatusFilter().contains(status).should('exist').click()
+  }
+
+  // gets given column of transactions in the transaction table such as, origin chain, date, status, etc
+  getTransactionsColumn(columnName: string) {
+    return this.withinPage()
+      .find('#transactionList', { timeout: 90 * 1000 })
+      .should('exist')
+      .find(`div[aria-label=${columnName}]`)
+      .should('exist')
+  }
+
+  checkStatusFilter() {
+    this.changeStatusFilter(TRANSACTION_FILTER_STATUS.Completed)
+    this.verifyFilteredTransactionsByColumn(
+      'transactionStatus',
+      TRANSACTION_FILTER_STATUS.Completed
+    )
+
+    this.changeStatusFilter(TRANSACTION_FILTER_STATUS.Pending)
+    this.verifyFilteredTransactionsByColumn(
+      'transactionStatus',
+      TRANSACTION_FILTER_STATUS.Pending
+    )
+
+    this.changeStatusFilter(TRANSACTION_FILTER_STATUS.Canceled)
+    this.changeStatusFilter(TRANSACTION_FILTER_STATUS.All)
+  }
+
+  checkConjunctionOfFilters() {
+    const orignalFromDate = this.fromDate
+
+    // use date pickers to filter by date
+    this.changeDatePicker(true, new Date('30 Jun 2023 12:00 AM'))
+
+    // use status filter to filter by status
+    this.changeStatusFilter(TRANSACTION_FILTER_STATUS.Completed)
+
+    // use network dropdowns to filter by network
+    this.changeFromNetwork(CHAIN_NAME.Goerli)
+
+    // verify transactions
+    this.verifyTransactionsByDate(this.fromDate, this.toDate)
+
+    this.verifyFilteredTransactionsByColumn(
+      'transactionStatus',
+      TRANSACTION_FILTER_STATUS.Completed
+    )
+
+    this.verifyFilteredTransactionsByColumn(
+      'transactionOriginDetails',
+      CHAIN_NAME.Goerli
+    )
+
+    this.changeDatePicker(true, orignalFromDate)
+
+    this.changeStatusFilter(TRANSACTION_FILTER_STATUS.All)
+    this.changeFromNetwork('All Networks')
+  }
+}
