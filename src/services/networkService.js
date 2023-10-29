b4de6d81fbea502df20e0e40ded0d4425814249e
Sahil Kashetwar <sahil@enya.ai>
2023-10-29 10:02:01 +0530

    fix: reverted indentatin change

--- src/services/networkService.js
+++ src/services/networkService.js
@@ -15,7 +15,7 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 import {formatEther, parseEther} from '@ethersproject/units'
-import {CrossChainMessenger} from '@bobanetwork/sdk'
+import {CrossChainMessenger,} from '@bobanetwork/sdk'
 
 import {BigNumber, ethers, utils} from 'ethers'
 
