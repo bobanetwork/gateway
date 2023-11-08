c8c07621cedd77048a6a1ba5bff07cc256d3b721
alvaro-ricotta <alvaro.e.ricotta@gmail.com>
2023-11-08 16:42:40 -0300

    adding unit test for walletSwitch

---
+++ src/hooks/useWalletSwitch/index.test.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { renderHook, act } from '@testing-library/react-hooks'
+import useWalletSwitch from '.'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import { mockedInitialState } from 'util/tests'
+import { Provider } from 'react-redux'
+
+describe('useWalletSwitch', () => {
+  const middlewares = [thunk]
+  const mockStore = configureMockStore(middlewares)
+
+  test('As default accountEnabled should be null and we should set ActiveNetwork, baseState, and Enable account', async () => {
+    const store = mockStore(mockedInitialState)
+    const wrapper = ({ children }) => (
+      <Provider store={store}>{children}</Provider>
+    )
+
+    renderHook(() => useWalletSwitch(), {
+      wrapper,
+    })
+
+    const actions = store.getActions()
+    expect(actions).toEqual([
+      { type: 'NETWORK/SET/ACTIVE' },
+      { payload: false, type: 'SETUP/BASE/SET' },
+      { payload: false, type: 'SETUP/ACCOUNT/SET' },
+    ])
+  })
+
+  test('When account is enabled and activeNetwork or activeNetworkType do not match, we open switch network modal', async () => {
+    const initialState = {
+      ...mockedInitialState,
+      setup: {
+        ...mockedInitialState.ui,
+        accountEnabled: true,
+      },
+    }
+    const store = mockStore(initialState)
+    const wrapper = ({ children }) => (
+      <Provider store={store}>{children}</Provider>
+    )
+
+    renderHook(() => useWalletSwitch(), {
+      wrapper,
+    })
+
+    const actions = store.getActions()
+    expect(actions).toContainEqual({
+      type: 'UI/MODAL/OPEN',
+      payload: 'switchNetworkModal',
+    })
+  })
+})
