a79a67e52c274778f26385f977f751f53ca990f1
alvaro-ricotta <alvaro.e.ricotta@gmail.com>
2023-11-09 12:46:04 -0300

    updating tests

--- src/hooks/useWalletSwitch/index.test.tsx
+++ src/hooks/useWalletSwitch/index.test.tsx
@@ -5,6 +5,7 @@ import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 import { mockedInitialState } from 'util/tests'
 import { Provider } from 'react-redux'
+import { LAYER } from 'util/constant'
 
 describe('useWalletSwitch', () => {
   const middlewares = [thunk]
@@ -50,5 +51,36 @@ describe('useWalletSwitch', () => {
       type: 'UI/MODAL/OPEN',
       payload: 'switchNetworkModal',
     })
+  })
+
+  test('When base enabled & reconnect', async () => {
+    const setReconnectMock = jest.fn()
+    const useStateMock = jest.spyOn(React, 'useState')
+    useStateMock.mockImplementation(() => [true, setReconnectMock])
+
+    const initialState = {
+      ...mockedInitialState,
+      setup: {
+        ...mockedInitialState.ui,
+        baseEnabled: true,
+      },
+    }
+
+    const store = mockStore(initialState)
+    const wrapper = ({ children }) => (
+      <Provider store={store}>{children}</Provider>
+    )
+
+    renderHook(() => useWalletSwitch(), {
+      wrapper,
+    })
+
+    const actions = store.getActions()
+    expect(actions).toEqual([
+      { payload: true, type: 'SETUP/CONNECT' },
+      { type: 'NETWORK/SET/ACTIVE' },
+      { payload: false, type: 'SETUP/BASE/SET' },
+      { payload: false, type: 'SETUP/ACCOUNT/SET' },
+    ])
   })
 })
