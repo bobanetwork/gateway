c8a243e95bf47496ff6f5627b9694eae7c670867
Sahil Kashetwar <sahil@enya.ai>
2023-11-11 15:07:40 +0530

    fix: broken integration test

--- cypress/e2e/helpers/bridge.ts
+++ cypress/e2e/helpers/bridge.ts
@@ -195,7 +195,7 @@ export default class Bridge extends Page {
       this.withinPage().contains(l2Networks[i].networkName).should('exist')
 
       this.openNetworkModal(l1Networks[i].networkName)
-      const nextNetwork = l1Networks[(i + 1) % 3]
+      const nextNetwork = l1Networks[(i + 1) % 2]
       this.selectNetworkFromModal(nextNetwork.networkName)
       if (accountConnected) {
         this.handleNetworkSwitchModals(
