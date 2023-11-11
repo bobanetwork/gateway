961b8cf7df432260fd4c6302e2e5cf9b9c25ab7b
Sahil Kashetwar <sahil@enya.ai>
2023-11-11 14:19:16 +0530

    useWalletSwitch 100% test coverage

--- src/hooks/useWalletSwitch/index.test.tsx
+++ src/hooks/useWalletSwitch/index.test.tsx
@@ -11,76 +11,279 @@ describe('useWalletSwitch', () => {
   const middlewares = [thunk]
   const mockStore = configureMockStore(middlewares)
 
-  test('As default accountEnabled should be null and we should set ActiveNetwork, baseState, and Enable account', async () => {
-    const store = mockStore(mockedInitialState)
-    const wrapper = ({ children }) => (
-      <Provider store={store}>{children}</Provider>
-    )
+  describe('When accountEnabled is falsy', () => {
+    test('Should set baseState, activenetwork, enableAccount when activeNetwork changes', async () => {
+      const store = mockStore({
+        ...mockedInitialState,
+        network: {
+          ...mockedInitialState.network,
+          network: 'BNB',
+          networkIcon: 'bnb',
+          networkType: 'Mainnet',
+          name: {},
+        },
+      })
+      const wrapper = ({ children }) => (
+        <Provider store={store}>{children}</Provider>
+      )
 
-    renderHook(() => useWalletSwitch(), {
-      wrapper,
+      renderHook(() => useWalletSwitch(), {
+        wrapper,
+      })
+
+      const actions = store.getActions()
+
+      expect(actions).toEqual([
+        { type: 'NETWORK/SET/ACTIVE' },
+        { payload: false, type: 'SETUP/BASE/SET' },
+        { payload: false, type: 'SETUP/ACCOUNT/SET' },
+      ])
     })
+    test('Should set baseState, activenetwork, enableAccount when activeNetworkType changes', async () => {
+      const store = mockStore({
+        ...mockedInitialState,
+        network: {
+          ...mockedInitialState.network,
+          network: 'ETHEREUM',
+          networkIcon: 'ethereum',
+          networkType: 'Testnet',
+          name: {},
+        },
+      })
+      const wrapper = ({ children }) => (
+        <Provider store={store}>{children}</Provider>
+      )
 
-    const actions = store.getActions()
-    expect(actions).toEqual([
-      { type: 'NETWORK/SET/ACTIVE' },
-      { payload: false, type: 'SETUP/BASE/SET' },
-      { payload: false, type: 'SETUP/ACCOUNT/SET' },
-    ])
+      renderHook(() => useWalletSwitch(), {
+        wrapper,
+      })
+
+      const actions = store.getActions()
+
+      expect(actions).toEqual([
+        { type: 'NETWORK/SET/ACTIVE' },
+        { payload: false, type: 'SETUP/BASE/SET' },
+        { payload: false, type: 'SETUP/ACCOUNT/SET' },
+      ])
+    })
+    test('Should not update state when activeNetwork/activeNetworkType not changes', async () => {
+      const store = mockStore({
+        ...mockedInitialState,
+        network: {
+          ...mockedInitialState.network,
+          network: 'ETHEREUM',
+          networkIcon: 'ethereum',
+          networkType: 'Mainnet',
+          name: {},
+        },
+      })
+      const wrapper = ({ children }) => (
+        <Provider store={store}>{children}</Provider>
+      )
+      renderHook(() => useWalletSwitch(), {
+        wrapper,
+      })
+      const actions = store.getActions()
+      expect(actions).toEqual([])
+    })
+  })
+  describe('When accountEnabled is truthy', () => {
+    test('Should open switchNetworkModal when activeNetwork changes', async () => {
+      const store = mockStore({
+        ...mockedInitialState,
+        setup: {
+          ...mockedInitialState.setup,
+          accountEnabled: true,
+        },
+        network: {
+          ...mockedInitialState.network,
+          network: 'BNB',
+          networkIcon: 'bnb',
+          networkType: 'Mainnet',
+          name: {},
+        },
+      })
+      const wrapper = ({ children }) => (
+        <Provider store={store}>{children}</Provider>
+      )
+
+      renderHook(() => useWalletSwitch(), {
+        wrapper,
+      })
+
+      const actions = store.getActions()
+
+      expect(actions).toEqual([
+        {
+          destNetworkSelection: undefined,
+          fast: undefined,
+          lock: undefined,
+          payload: 'switchNetworkModal',
+          proposalId: undefined,
+          selectionLayer: undefined,
+          token: undefined,
+          tokenIndex: undefined,
+          type: 'UI/MODAL/OPEN',
+        },
+      ])
+    })
+    test('Should open switchNetworkModal when activeNetworkType changes', async () => {
+      const store = mockStore({
+        ...mockedInitialState,
+        setup: {
+          ...mockedInitialState.setup,
+          accountEnabled: true,
+        },
+        network: {
+          ...mockedInitialState.network,
+          network: 'ETHEREUM',
+          networkIcon: 'ethereum',
+          networkType: 'Testnet',
+          name: {},
+        },
+      })
+      const wrapper = ({ children }) => (
+        <Provider store={store}>{children}</Provider>
+      )
+
+      renderHook(() => useWalletSwitch(), {
+        wrapper,
+      })
+
+      const actions = store.getActions()
+
+      expect(actions).toEqual([
+        {
+          destNetworkSelection: undefined,
+          fast: undefined,
+          lock: undefined,
+          payload: 'switchNetworkModal',
+          proposalId: undefined,
+          selectionLayer: undefined,
+          token: undefined,
+          tokenIndex: undefined,
+          type: 'UI/MODAL/OPEN',
+        },
+      ])
+    })
+    test('Should not update state when activeNetwork / activeNetworkType not changed', async () => {
+      const store = mockStore({
+        ...mockedInitialState,
+        setup: {
+          ...mockedInitialState.setup,
+          accountEnabled: true,
+        },
+        network: {
+          ...mockedInitialState.network,
+          network: 'ETHEREUM',
+          networkIcon: 'ethereum',
+          networkType: 'Mainnet',
+          name: {},
+        },
+      })
+      const wrapper = ({ children }) => (
+        <Provider store={store}>{children}</Provider>
+      )
+      renderHook(() => useWalletSwitch(), {
+        wrapper,
+      })
+      const actions = store.getActions()
+      expect(actions).toEqual([])
+    })
   })
 
-  test('When account is enabled and activeNetwork or activeNetworkType do not match, we open switch network modal', async () => {
-    const initialState = {
-      ...mockedInitialState,
-      setup: {
-        ...mockedInitialState.ui,
-        accountEnabled: true,
-      },
-    }
-    const store = mockStore(initialState)
-    const wrapper = ({ children }) => (
-      <Provider store={store}>{children}</Provider>
-    )
+  describe('Should trigger connect when base enabled & reconnect', () => {
+    test('should dispatch connectETH on layer L1', async () => {
+      const setReconnectMock = jest.fn()
+      const useStateMock = jest.spyOn(React, 'useState')
+      useStateMock.mockImplementation(() => [true, setReconnectMock])
 
-    renderHook(() => useWalletSwitch(), {
-      wrapper,
+      const initialState = {
+        ...mockedInitialState,
+        setup: {
+          ...mockedInitialState.setup,
+          baseEnabled: true,
+        },
+      }
+
+      const store = mockStore(initialState)
+      const wrapper = ({ children }) => (
+        <Provider store={store}>{children}</Provider>
+      )
+
+      renderHook(() => useWalletSwitch(), {
+        wrapper,
+      })
+
+      const actions = store.getActions()
+      expect(actions).toEqual([
+        { payload: true, type: 'SETUP/CONNECT_ETH' },
+        { type: 'NETWORK/SET/ACTIVE' },
+        { payload: false, type: 'SETUP/BASE/SET' },
+        { payload: false, type: 'SETUP/ACCOUNT/SET' },
+      ])
     })
+    test('should dispatch connectBOBA on layer L2', async () => {
+      const setReconnectMock = jest.fn()
+      const useStateMock = jest.spyOn(React, 'useState')
+      useStateMock.mockImplementation(() => [true, setReconnectMock])
 
-    const actions = store.getActions()
-    expect(actions).toContainEqual({
-      type: 'UI/MODAL/OPEN',
-      payload: 'switchNetworkModal',
+      const initialState = {
+        ...mockedInitialState,
+        setup: {
+          ...mockedInitialState.setup,
+          netLayer: 'L2',
+          baseEnabled: true,
+        },
+      }
+
+      const store = mockStore(initialState)
+      const wrapper = ({ children }) => (
+        <Provider store={store}>{children}</Provider>
+      )
+
+      renderHook(() => useWalletSwitch(), {
+        wrapper,
+      })
+
+      const actions = store.getActions()
+      expect(actions).toEqual([
+        { payload: true, type: 'SETUP/CONNECT_BOBA' },
+        { type: 'NETWORK/SET/ACTIVE' },
+        { payload: false, type: 'SETUP/BASE/SET' },
+        { payload: false, type: 'SETUP/ACCOUNT/SET' },
+      ])
     })
-  })
+    test('should dispatch connect on layer is not L1 or L2', async () => {
+      const setReconnectMock = jest.fn()
+      const useStateMock = jest.spyOn(React, 'useState')
+      useStateMock.mockImplementation(() => [true, setReconnectMock])
 
-  test('When base enabled & reconnect', async () => {
-    const setReconnectMock = jest.fn()
-    const useStateMock = jest.spyOn(React, 'useState')
-    useStateMock.mockImplementation(() => [true, setReconnectMock])
+      const initialState = {
+        ...mockedInitialState,
+        setup: {
+          ...mockedInitialState.setup,
+          netLayer: null,
+          baseEnabled: true,
+        },
+      }
 
-    const initialState = {
-      ...mockedInitialState,
-      setup: {
-        ...mockedInitialState.ui,
-        baseEnabled: true,
-      },
-    }
+      const store = mockStore(initialState)
+      const wrapper = ({ children }) => (
+        <Provider store={store}>{children}</Provider>
+      )
 
-    const store = mockStore(initialState)
-    const wrapper = ({ children }) => (
-      <Provider store={store}>{children}</Provider>
-    )
+      renderHook(() => useWalletSwitch(), {
+        wrapper,
+      })
 
-    renderHook(() => useWalletSwitch(), {
-      wrapper,
+      const actions = store.getActions()
+      expect(actions).toEqual([
+        { payload: true, type: 'SETUP/CONNECT' },
+        { type: 'NETWORK/SET/ACTIVE' },
+        { payload: false, type: 'SETUP/BASE/SET' },
+        { payload: false, type: 'SETUP/ACCOUNT/SET' },
+      ])
     })
-
-    const actions = store.getActions()
-    expect(actions).toEqual([
-      { payload: true, type: 'SETUP/CONNECT' },
-      { type: 'NETWORK/SET/ACTIVE' },
-      { payload: false, type: 'SETUP/BASE/SET' },
-      { payload: false, type: 'SETUP/ACCOUNT/SET' },
-    ])
   })
 })
