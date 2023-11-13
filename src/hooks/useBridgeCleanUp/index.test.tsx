7ab3c106663516bc15a02d8e2200e62c2eeccf9d
alvaro-ricotta <alvaro.e.ricotta@gmail.com>
2023-11-13 14:42:02 -0300

    cleanup bridge

---
+++ src/hooks/useBridgeCleanUp/index.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import configureMockStore from 'redux-mock-store'
+import { renderHook } from '@testing-library/react-hooks'
+import thunk from 'redux-thunk'
+import { useBridgeCleanup } from './'
+import { mockedInitialState } from 'util/tests'
+import { AnyAction } from 'redux'
+
+describe('useBridgeCleanUp', () => {
+  const middlewares = [thunk]
+  const mockStore = configureMockStore(middlewares)
+
+  test('Should cleanUp bridge', async () => {
+    const store = mockStore(mockedInitialState)
+    const wrapper = ({ children }) => (
+      <Provider store={store}>{children}</Provider>
+    )
+    renderHook(() => useBridgeCleanup(), {
+      wrapper,
+    })
+
+    const actions: AnyAction[] = store.getActions()
+    expect(actions).toContainEqual({
+      type: 'BRIDGE/TOKEN/RESET',
+    })
+    expect(actions).toContainEqual({
+      type: 'BRIDGE/ALERT/PURGE',
+    })
+    expect(actions).toContainEqual({
+      type: 'BRIDGE/AMOUNT/RESET',
+    })
+  })
+})
