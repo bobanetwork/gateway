491136e76a94cbdde698b608e6059e82ac956a66
wsdt <kevin.riedl@wavect.io>
2023-11-14 04:43:49 +0100

    fix: Type layout partially

--- src/actions/uiAction.ts
+++ src/actions/uiAction.ts
@@ -13,8 +13,10 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-export const setTheme = (theme) => (dispatch) =>
-  dispatch({ type: 'UI/THEME/UPDATE', payload: theme })
+export const setTheme =
+  (theme): any =>
+  (dispatch) =>
+    dispatch({ type: 'UI/THEME/UPDATE', payload: theme })
 
 export const openModal =
   (
