39e9a1d8e33fd6ca86d6ff3295d5735957f780bc
wsdt <kevin.riedl@wavect.io>
2023-11-16 12:40:27 +0100

    undo cypress changes

--- cypress/e2e/helpers/bridge.ts
+++ cypress/e2e/helpers/bridge.ts
@@ -12,6 +12,36 @@ export default class Bridge extends Page {
     this.title = 'Bridge'
   }
 
+  switchNetworkType(network: string, isTestnet: boolean, newNetwork: boolean) {
+    this.withinPage().find('#settings').should('exist').click()
+    cy.get('label[title="testnetSwitch"]').should('exist').click()
+
+    this.store.verifyReduxStoreNetwork(
+      'activeNetworkType',
+      isTestnet ? 'Testnet' : 'Mainnet'
+    )
+
+    this.handleNetworkSwitchModals(network, isTestnet)
+
+    if (newNetwork) {
+      this.allowNetworkToBeAddedAndSwitchedTo()
+    } else {
+      this.allowNetworkSwitch()
+    }
+
+    this.checkNetworkSwitchSuccessful(network)
+  }
+
+  switchBridgeDirection(newOriginLayer: Layer, newNetwork: boolean) {
+    this.withinPage().find('#switchBridgeDirection').should('exist').click()
+    if (newNetwork) {
+      this.allowNetworkToBeAddedAndSwitchedTo()
+    } else {
+      this.allowNetworkSwitch()
+    }
+    this.store.verifyReduxStoreSetup('netLayer', newOriginLayer)
+  }
+
   selectToken(tokenSymbol: string) {
     this.withinPage().contains('Select').should('exist').click()
 
@@ -37,6 +67,60 @@ export default class Bridge extends Page {
       .should('be.visible')
       .and('have.prop', 'naturalWidth')
       .should('be.greaterThan', 0)
+  }
+
+  bridgeToken(tokenSymbol: string, amount: string, destinationLayer: Layer) {
+    this.selectToken(tokenSymbol)
+    if (destinationLayer === Layer.L1) {
+      this.store
+        .getReduxStore()
+        .its('setup')
+        .its('bobaFeePriceRatio')
+        .should('not.be.empty')
+
+      this.store.verifyReduxStoreSetup('netLayer', Layer.L2)
+      this.store
+        .getReduxStore()
+        .its('balance')
+        .its('exitFee')
+        .should('not.be.empty')
+      this.store
+        .getReduxStore()
+        .its('balance')
+        .its('classicExitCost')
+        .should('equal', 0)
+    }
+
+    cy.get(`input[placeholder="Amount to bridge to ${destinationLayer}"]`)
+      .should('exist')
+      .focus()
+      .type(`${amount}`)
+
+    this.store
+      .getReduxStore()
+      .its('bridge')
+      .its('amountToBridge')
+      .then(parseFloat)
+      .should('equal', parseFloat(amount))
+
+    cy.get('button').contains('Bridge').should('exist').click()
+    cy.contains(`${amount} ${tokenSymbol}`, { timeout: 60000 }).should('exist')
+    cy.get('button').contains('Confirm').should('exist').click()
+    if (destinationLayer === Layer.L1) {
+      this.allowMetamaskToSpendToken('10')
+    }
+    this.confirmTransactionOnMetamask()
+    if (destinationLayer === Layer.L2) {
+      this.getModal().contains('Estimated time to complete :').should('exist')
+    } else {
+      this.getModal()
+        .contains('Your funds will arrive in 7 days at your wallet on')
+        .should('exist')
+    }
+    this.getModal()
+      .find('div[aria-label=closeModalIcon]')
+      .should('have.length', 1)
+      .click()
   }
 
   checkThirdPartyTabInETH() {
