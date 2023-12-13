5da66751d67ad3fa8099ba0f6def2155f2360c20
Sahil Kashetwar <sahil@enya.ai>
2023-12-13 14:18:28 +0530

    Earn service structure or outline

---
+++ src/services/earn.service.ts
@@ -0,0 +1,21 @@
+/* eslint-disable */
+
+/*Earn program is deprecated but we need to support indefinetly to withdraw tokens from LP*/
+
+class EarnService {
+  async getL1LPInfo() {}
+
+  async getL2LPInfo() {}
+
+  async getL1LPBalance() {}
+
+  async getL2LPBalance() {}
+
+  async withdrawReward() {}
+
+  async withdrawLiquidity() {}
+}
+
+const earnService = new EarnService()
+
+export default earnService
