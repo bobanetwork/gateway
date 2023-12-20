a4ac296e81d4d5a8bad0d6a6ce4efbf030d054f7
jemeza <57341979+jemeza@users.noreply.github.com>
2023-12-20 17:00:19 -0600

    finishing styling fix and refactor

--- src/components/NetworkSelector/index.tsx
+++ src/components/NetworkSelector/index.tsx
@@ -1,37 +1,17 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
-  selectActiveNetwork,
   selectActiveNetworkType,
-  selectActiveNetworkName,
   selectNetworkChainIds,
-  selectModalState,
   selectLayer,
-  selectChainIdChanged,
   selectDestChainIdTeleportation,
   selectBridgeType,
 } from 'selectors'
-import {
-  INetwork,
-  L1_ICONS,
-  L2_ICONS,
-  NetworkList,
-  CHAIN_ID_LIST,
-} from 'util/network/network.util'
+import { NetworkList, CHAIN_ID_LIST } from 'util/network/network.util'
 import { setNetwork } from 'actions/networkAction'
 import { useDispatch, useSelector } from 'react-redux'
 import { setTeleportationDestChainId } from 'actions/bridgeAction'
-import { ChainLabel } from 'components/bridge/ChainLabel'
-import {
-  NetworkContainer,
-  Arrow,
-  Dropdown,
-  NetworkSelectorDropdown,
-} from './styles'
-import { getCoinImage } from 'util/coinImage'
-import ArrowDown from 'assets/images/icons/arrowdown.svg'
-// import { NetworkList } from 'components/bridge/NetworkPickerList'
-import { Typography } from 'components/global/'
-import networkReducer from 'reducers/networkReducer'
+import { BRIDGE_TYPE } from 'containers/Bridging/BridgeTypeSelector'
+import { NetworkSelectorDropdown } from './styles'
 import { IDropdownItem } from 'components/global/dropdown'
 
 const NetworkDropdownHeading = {
@@ -45,18 +25,19 @@ export const NetworkSelector = () => {
   const networkType = useSelector(selectActiveNetworkType())
   const layer = useSelector(selectLayer())
   const networkChainIds = useSelector(selectNetworkChainIds())
+  const bridgeType = useSelector(selectBridgeType())
+
+  const [currentOption, setCurrentOption] = useState<any>({
+    label: CHAIN_ID_LIST[1].siteName,
+    value: String(1),
+    imgSrc: CHAIN_ID_LIST[1].imgSrc,
+    imgType: 'img',
+  })
 
   const currentNetworkIsL1 = layer === 'L1'
-  const currentNetworkChainId = Number(networkChainIds[layer])
-  const currentNetwork = {
-    label: CHAIN_ID_LIST[currentNetworkChainId].siteName,
-    value: String(currentNetworkChainId),
-    imgSrc: CHAIN_ID_LIST[currentNetworkChainId].imgSrc,
-    imgType: 'img',
-  }
-  const currTeleportationDestChainId = useSelector(
-    selectDestChainIdTeleportation()
-  )
+
+  const lightBridgeInUse = BRIDGE_TYPE.LIGHT === bridgeType
+
   const onSelect = (chainId: string | undefined) => {
     if (!chainId) {
       return
@@ -71,18 +52,33 @@ export const NetworkSelector = () => {
         networkType,
       })
     )
-    if (chainDetail.chainId === currTeleportationDestChainId) {
-      dispatch(
-        setTeleportationDestChainId(
-          chainDetail.chainId[layer?.toUpperCase() === 'L1' ? 'L2' : 'L1']
-        )
-      )
-    }
   }
 
+  useEffect(() => {
+    if (networkChainIds) {
+      const chainId = Number(networkChainIds[layer])
+      setCurrentOption({
+        label: CHAIN_ID_LIST[chainId].siteName,
+        value: String(chainId),
+        imgSrc: CHAIN_ID_LIST[chainId].imgSrc,
+        imgType: 'img',
+      })
+    }
+  }, [networkChainIds, layer])
+
   const getNetworkItems = () => {
-    const networksList =
-      networkType === 'Testnet' ? NetworkList.Testnet : NetworkList.Mainnet
+    let networksList =
+      networkType === 'Testnet'
+        ? [...NetworkList.Testnet]
+        : [...NetworkList.Mainnet]
+
+    networksList = networksList.filter((network) => {
+      if (network.limitedAvailability) {
+        return lightBridgeInUse
+      }
+      return true
+    })
+
     let networkOptions = networksList.map((network) => {
       const networkName = currentNetworkIsL1 ? network.name.l1 : network.name.l2
       const chainId = currentNetworkIsL1
@@ -103,14 +99,13 @@ export const NetworkSelector = () => {
       )
       return firstOccurence === index
     })
-
     return [NetworkDropdownHeading, ...networkOptions]
   }
 
   return (
     <NetworkSelectorDropdown
       items={getNetworkItems() as IDropdownItem[]}
-      defaultItem={currentNetwork}
+      defaultItem={currentOption}
       onItemSelected={(item) => onSelect(item.value)}
       setSelectedOnClick={false}
       error={false}
--- src/components/NetworkSelector/styles.ts
+++ src/components/NetworkSelector/styles.ts
@@ -1,7 +1,4 @@
 import styled, { css } from 'styled-components'
-import { Svg } from 'components/global/svg'
-import { mobile } from 'themes/screens'
-import { ChainLabelContainer } from 'components/bridge/ChainLabel/styles'
 import { FeeSwitchterDropdown } from 'components/layout/Header/feeSwitcher/styles'
 import {
   Header,
@@ -9,92 +6,9 @@ import {
   Option,
 } from 'components/global/dropdown/styles'
 
-export const NetworkContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 4px 24px;
-  border-radius: 33px;
-  gap: 10px;
-  user-select: none;
-  height: 40px;
-  cursor: pointer;
-  ${(props) =>
-    props.theme.name === 'light' &&
-    css`
-      background: #fff;
-    `}
-  ${(props) =>
-    props.theme.name === 'dark' &&
-    css`
-      background: ${props.theme.colors.gray[400]};
-    `}
-
-    ${mobile(css`
-    ${ChainLabelContainer} {
-      font-size: 0px;
-      gap: 0px;
-    }
-  `)}
-`
-
-export const Arrow = styled(Svg)`
-  display: flex;
-  > div {
-    display: flex;
-    height: auto;
-    padding: 5px 0px;
-  }
-  ${(props) =>
-    props.theme.name === 'light' &&
-    css`
-      svg {
-        fill: ${props.theme.colors.gray[800]};
-      }
-    `}
-  ${(props) =>
-    props.theme.name === 'dark' &&
-    css`
-      svg {
-        fill: ${props.theme.colors.gray[50]};
-      }
-    `}
-`
-
-export const Dropdown = styled.div`
-  display: flex;
-  flex-direction: column;
-  position: absolute;
-  top: 65px;
-  border-radius: 8px;
-  background: inherit;
-  gap: 5px;
-  padding: 10px 15px;
-  img,
-  svg {
-    max-width: 24px;
-    width: 100%;
-    height: 100%;
-    max-height: 34px;
-  }
-  > div {
-    > div {
-      padding: 5px 10px;
-    }
-  }
-  ${mobile(css`
-    width: 100%;
-    position: fixed;
-    left: 0px;
-    padding: 25px;
-    border-radius: 0px;
-    top: 72px;
-  `)}
-`
-
 export const NetworkSelectorDropdown = styled(FeeSwitchterDropdown)`
   ${Header} {
-    width: 172px;
+    width: 200px;
     height: 40px;
     img {
       border-radius: 50%;
@@ -108,7 +22,12 @@ export const NetworkSelectorDropdown = styled(FeeSwitchterDropdown)`
     gap: 8px;
     min-height: unset;
     max-height: 238px;
-    max-width: 184px;
+    max-width: 186px;
+    ${(props) =>
+      props.theme.name === 'dark' &&
+      css`
+        color: ${props.theme.colors.gray[100]};
+      `}
     ${(props) =>
       props.theme.name === 'light' &&
       css`
@@ -116,13 +35,8 @@ export const NetworkSelectorDropdown = styled(FeeSwitchterDropdown)`
         border: 1px solid ${props.theme.colors.gray[400]};
       `}
     & > ${Option} {
-      width: 145px;
+      width: 168px;
       height: 32px;
-      ${(props) =>
-        props.theme.name === 'dark' &&
-        css`
-          color: ${props.theme.colors.gray[100]};
-        `}
     }
   }
 `
