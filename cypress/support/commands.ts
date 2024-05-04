8f2461c38cd826d5240fb2361f664a17242db004
Sahil Kashetwar <sahil@enya.ai>
2024-05-04 17:13:29 +0530

    change: update version and added ref types.

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,4 +1,5 @@
 /// <reference types="cypress" / >
+/// <reference types="@testing-library/cypress" / >
 // ***********************************************
 // This example commands.ts shows you how to
 // create various custom commands and overwrite
