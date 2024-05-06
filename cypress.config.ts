559e8bae6503eb9b2d58a943d34cdd2b9e0dd60d
Sahil Kashetwar <sahil@enya.ai>
2024-05-06 18:05:30 +0530

    fix the cypress & synpress integration test runing with old packages

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,18 +1,43 @@
+// import { defineConfig } from 'cypress';
+
+// export default defineConfig({
+//   chromeWebSecurity: true,
+//   defaultCommandTimeout: 60000,
+//   pageLoadTimeout: 60000,
+//   requestTimeout: 60000,
+//   video: true,
+//   videosFolder: 'cypress/videos',
+//   e2e: {
+//     testIsolation: false,
+//     // setupNodeEvents(on, config) {
+//     // // implement node event listeners here
+//     // },
+//     baseUrl: 'http://localhost:3000',
+//     // baseUrl: "https://staging.gateway.boba.network"
+//   },
+// })
+
 import { defineConfig } from 'cypress'
+import synpressPlugins from '@synthetixio/synpress/plugins'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 export default defineConfig({
+  userAgent: 'synpress',
   chromeWebSecurity: true,
-  defaultCommandTimeout: 60000,
-  pageLoadTimeout: 60000,
-  requestTimeout: 60000,
-  video: true,
-  videosFolder: 'cypress/videos',
+  defaultCommandTimeout: 30000,
+  pageLoadTimeout: 30000,
+  requestTimeout: 30000,
   e2e: {
     testIsolation: false,
-    // setupNodeEvents(on, config) {
-    // // implement node event listeners here
-    // },
+    setupNodeEvents: (on, config) => {
+      synpressPlugins(on, config)
+    },
     baseUrl: 'http://localhost:3000',
-    // baseUrl: "https://staging.gateway.boba.network"
+    supportFile: 'cypress/support/e2e.ts',
+    specPattern: ['cypress/e2e/**/*.spec.cy.ts'],
   },
+  // env: {
+  //   target_hash: process.env.CYPRESS_TEST_HASH,
+  // },
 })
