e92653035cf1f5257e7906651b09ac653b506979
Sahil Kashetwar <sahil@enya.ai>
2024-07-14 15:35:05 +0530

    override forbid only for test

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
     timeout: 5000,
   },
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
   workers: 1,
   reporter: [['list'], ['dot'], ['html']],
