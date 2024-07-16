aeb5a14740df8f6b775e537ba94eb6dec8348dbc
Sahil Kashetwar <sahil@enya.ai>
2024-07-16 18:53:07 +0530

    updated config: - forbid only to null - viewport to null - only one test.

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   fullyParallel: true,
   retries: process.env.CI ? 1 : 0,
   workers: 1,
-  forbidOnly: !!process.env.CI,
+  // forbidOnly: !!process.env.CI,
   reporter: [['list'], ['dot'], ['html']],
   use: {
     headless: !!process.env.CI,
@@ -37,11 +37,11 @@ export default defineConfig({
     {
       name: 'chromium',
       use: {
-        // viewport: null,
-        ...devices['Desktop Chrome'],
+        viewport: null,
+        // ...devices['Desktop Chrome'],
         // It is important to define the `viewport` property after destructuring `devices`,
         // since devices also define the `viewport` for that device.
-        viewport: { width: 1280, height: 850 },
+        // viewport: { width: 1280, height: 850 },
       },
     },
   ],
