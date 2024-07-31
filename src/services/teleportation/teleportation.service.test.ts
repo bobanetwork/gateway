0d92fd7e752f7c333e1540677748cbbcf6fb45c4
Sahil Kashetwar <sahil@enya.ai>
2024-08-01 02:20:52 +0530

    change: teleporation service unit test coverage

--- src/services/teleportation/teleportation.service.test.ts
+++ src/services/teleportation/teleportation.service.test.ts
@@ -1,12 +1,37 @@
-import { CHAIN_ID_LIST } from 'util/network/network.util'
+import { CHAIN_ID_LIST, getRpcUrl } from 'util/network/network.util'
 import appService from '../app.service'
 import { MOCK_CHAIN_LIST } from '../mock/teleporation'
 import { lightBridgeService as lbs } from './teleportation.service'
 import networkService from '../networkService'
 import { Layer } from 'util/constant'
+import { constants, Contract, providers } from 'ethers'
+import { TeleportationABI } from 'services/abi'
 
+jest.mock('../../util/network/network.util', () => {
+  const originalModule = jest.requireActual('../../util/network/network.util')
+  return {
+    ...originalModule,
+    getRpcUrl: jest.fn(),
+  }
+})
 jest.mock('../networkService')
 jest.mock('../app.service')
+jest.mock('ethers', () => {
+  const originalModule = jest.requireActual('ethers')
+  return {
+    __esModule: true,
+    ...originalModule,
+    Contract: jest.fn(),
+    providers: {
+      JsonRpcProvider: jest.fn().mockReturnValue({
+        getSigner: jest.fn(),
+      }),
+      StaticJsonRpcProvider: jest.fn().mockReturnValue({
+        getSigner: jest.fn(),
+      }),
+    },
+  }
+})
 
 const addresses = {
   Proxy__L1Teleportation: 'L1_TELEPORTATION',
@@ -91,10 +116,17 @@ describe('TeleportationService', () => {
   })
 
   describe('GetLightBridgeContract', () => {
+    let mockSigner
     beforeEach(() => {
       jest.spyOn(lbs, 'getLightBridgeAddress').mockResolvedValue({
         lightBridgeAddress: addresses.Proxy__L1Teleportation,
         destChainConfig: MOCK_CHAIN_LIST[99],
+      })
+      mockSigner = jest.fn()
+      ;(
+        providers.StaticJsonRpcProvider as unknown as jest.Mock
+      ).mockReturnValue({
+        getSigner: mockSigner,
       })
     })
 
@@ -108,13 +140,238 @@ describe('TeleportationService', () => {
       expect(result).toBeNull()
     })
 
-    it('Should return contract correctly with rpc and provider', async () => {
+    it('Should return contract when the address is found', async () => {
+      const cMock = {
+        address: addresses.Proxy__L1Teleportation,
+      }
+      ;(Contract as unknown as jest.Mock).mockReturnValue(cMock)
       const result = await lbs.getLightBridgeContract(99)
-      expect(result?.provider['connection']).toEqual({
-        url: 'https://sepolia.boba.network',
-      })
-      expect(result?.address).toEqual(addresses.Proxy__L1Teleportation)
       expect(result).not.toBeNull()
+      expect(result).toBe(cMock)
+      expect(getRpcUrl).toHaveBeenCalledWith({
+        layer: 'L2',
+        network: 'ETHEREUM',
+        networkType: 'Testnet',
+      })
+      expect(Contract).toHaveBeenCalledWith(
+        'L1_TELEPORTATION',
+        TeleportationABI,
+        {
+          getSigner: mockSigner,
+        }
+      )
+    })
+  })
+
+  describe('isTokenSupported', () => {
+    let contractMock: any
+    let getSignerMock: any
+    const layer = Layer.L1
+    let tokenAdress = '0xValidTokenAddress'
+    const destChainId = '1'
+    beforeEach(() => {
+      getSignerMock = jest.fn().mockReturnValue({})
+      ;(providers.JsonRpcProvider as unknown as jest.Mock).mockReturnValue({
+        getSigner: getSignerMock,
+      })
+      contractMock = {
+        supportedTokens: jest.fn().mockResolvedValue({ supported: true }),
+      }
+      ;(Contract as unknown as jest.Mock).mockReturnValue(contractMock)
+      ;(networkService as any).provider = new providers.JsonRpcProvider(
+        'http://demo.local'
+      )
+
+      networkService.addresses = {
+        Proxy__L1Teleportation: 'lightBridgeAddressL1',
+        Proxy__L2Teleportation: 'lightBridgeAddressL2',
+      }
+    })
+    afterEach(() => {
+      jest.clearAllMocks()
+    })
+
+    it('should return supported status for L2 layer with valid token address', async () => {
+      const result = await lbs.isTokenSupported({
+        layer,
+        tokenAdress,
+        destChainId,
+      })
+      expect(result).toEqual({ supported: true })
+    })
+
+    it('should return supported status for L1 layer with valid token address', async () => {
+      const result = await lbs.isTokenSupported({
+        layer: 'L2',
+        tokenAdress,
+        destChainId,
+      })
+      expect(Contract).toHaveBeenCalledWith(
+        'lightBridgeAddressL2',
+        TeleportationABI,
+        {}
+      )
+      expect(result).toEqual({ supported: true })
+    })
+
+    it('should replace specific token address with AddressZero and return supported status', async () => {
+      tokenAdress = '0x4200000000000000000000000000000000000006'
+
+      const result = await lbs.isTokenSupported({
+        layer,
+        tokenAdress,
+        destChainId,
+      })
+      expect(result).toEqual({ supported: true })
+    })
+
+    it('should throw an error if lightBridgeAddress is invalid and supported to be false', async () => {
+      networkService.addresses = { Proxy__L1Teleportation: undefined }
+      const result = await lbs.isTokenSupported({
+        layer,
+        tokenAdress,
+        destChainId,
+      })
+      expect(result).toEqual({ supported: false })
+    })
+
+    it('should catch and return supported as false on contract call fails', async () => {
+      const error = new Error('Failed invoke contract')
+      contractMock.supportedTokens.mockResolvedValue(error)
+      const result = await lbs.isTokenSupported({
+        layer,
+        tokenAdress,
+        destChainId,
+      })
+
+      expect(result).toEqual(error)
+    })
+  })
+
+  describe('deposit', () => {
+    let contractMock: any
+    let getSignerMock: any
+    const lightBridgeAddress = '0xL2ProxyAddress'
+    const tokenAddress = '0xNativeTokenAddress'
+    const value = '1000'
+    const destChainId = 123
+    let layer = 'L1'
+
+    beforeEach(() => {
+      getSignerMock = jest.fn().mockReturnValue({})
+      ;(providers.JsonRpcProvider as unknown as jest.Mock).mockReturnValue({
+        getSigner: getSignerMock,
+      })
+      contractMock = {
+        supportedTokens: jest.fn().mockResolvedValue({ supported: true }),
+        teleportAsset: jest.fn().mockResolvedValue({ wait: jest.fn() }),
+      }
+      ;(Contract as unknown as jest.Mock).mockReturnValue(contractMock)
+      ;(networkService as any).provider = new providers.JsonRpcProvider(
+        'http://demo.local'
+      )
+
+      networkService.addresses = {
+        Proxy__L1Teleportation: lightBridgeAddress,
+        L1_ETH_Address: tokenAddress,
+        NETWORK_NATIVE_TOKEN: tokenAddress,
+      }
+    })
+    afterEach(() => {
+      jest.clearAllMocks()
+    })
+
+    it('should deposit correctly L1 -> L2 with NETWORK_NATIVE_TOKEN', async () => {
+      const txMock = { wait: jest.fn() }
+
+      contractMock.teleportAsset.mockResolvedValue(txMock)
+
+      const result = await lbs.deposit({
+        layer,
+        tokenAddress,
+        value,
+        destChainId,
+      })
+
+      expect(result).toBe(true)
+      expect(txMock.wait).toHaveBeenCalled()
+      expect(contractMock.supportedTokens).toHaveBeenCalledWith(
+        constants.AddressZero,
+        destChainId
+      )
+      expect(contractMock.teleportAsset).toHaveBeenCalledWith(
+        constants.AddressZero,
+        value,
+        destChainId,
+        { value }
+      )
+    })
+
+    it('should deposit correctly L1 -> L2 with Different Token', async () => {
+      layer = 'L2'
+
+      networkService.addresses = {
+        ...networkService.addresses,
+        NETWORK_NATIVE_TOKEN: 'BOBA',
+      }
+
+      const txMock = { wait: jest.fn() }
+
+      contractMock.teleportAsset.mockResolvedValue(txMock)
+
+      const result = await lbs.deposit({
+        layer,
+        tokenAddress,
+        value,
+        destChainId,
+      })
+
+      expect(result).toBe(true)
+      expect(txMock.wait).toHaveBeenCalled()
+      expect(contractMock.supportedTokens).toHaveBeenCalledWith(
+        tokenAddress,
+        destChainId
+      )
+      expect(contractMock.teleportAsset).toHaveBeenCalledWith(
+        tokenAddress,
+        value,
+        destChainId,
+        { value }
+      )
+    })
+
+    it('should handle unsupported token error', async () => {
+      networkService.addresses = {
+        ...networkService.addresses,
+        NETWORK_NATIVE_TOKEN: 'BOBA',
+      }
+      contractMock.supportedTokens.mockResolvedValue({ supported: false })
+
+      const result = await lbs.deposit({
+        layer,
+        tokenAddress,
+        value,
+        destChainId,
+      })
+      const error = new Error(
+        `GATEWAY ERROR: Asset ${tokenAddress} not supported for chain ${destChainId}`
+      )
+      expect(result).toEqual(error)
+    })
+
+    it('should handle deposit transaction error', async () => {
+      const error = new Error('Invalid txs!')
+
+      contractMock.teleportAsset.mockRejectedValue(error)
+
+      const result = await lbs.deposit({
+        layer,
+        tokenAddress,
+        value,
+        destChainId,
+      })
+
+      expect(result).toEqual(error)
     })
   })
 })
