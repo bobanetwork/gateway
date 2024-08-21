487c7bf2aae2e8a3944ebaf05837786b5f800a42
Sahil Kashetwar <sahil@enya.ai>
2024-08-21 19:06:46 +0530

    removed console

--- src/reducers/balanceReducer.ts
+++ src/reducers/balanceReducer.ts
@@ -37,8 +37,6 @@ const balanceReducer = (state: IBalanceReducerState = initialState, action) => {
   switch (action.type) {
     case 'BALANCE/GET/SUCCESS':
       const { layer1, layer2 } = action.payload
-      console.log(`layer2`, layer2)
-      console.log(`layer1`, layer1)
       return {
         ...state,
         layer1,
