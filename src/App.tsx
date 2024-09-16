a56178d12a82dac461355fc3e9d4358e74e2d61e
Sahil Kashetwar <sahil@enya.ai>
2024-09-16 15:32:45 +0530

    change: imported shadcn ui components.
    Alert-dialog | Avatar | Badge | Calendar | Card | Checkbox | Dialog | Drawer | Hover-card | Input | Label | Progress | Radio-group | Select | Sonner | Switch | Textarea | Tooltip

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { useAccount, useConnect, useDisconnect } from "wagmi";
-
+import { Button } from '@/components/ui/button'
+import { EnvelopeOpenIcon, ReloadIcon } from "@radix-ui/react-icons";
+import { Input } from "@/components/ui/input";
+import { Label } from "./components/ui/label";
 function App() {
   const account = useAccount();
   const { connectors, connect, status, error } = useConnect();
@@ -19,21 +22,41 @@ function App() {
         </div>
 
         {account.status === "connected" && (
-          <button type="button" onClick={() => disconnect()}>
+          <Button type="button" onClick={() => disconnect()}>
             Disconnect
-          </button>
+          </Button>
         )}
       </div>
 
       <div>
         <h2>Connect</h2>
-        {connectors.map((connector) => (
-          <button key={connector.uid} onClick={() => connect({ connector })} type="button">
+        {connectors.map((connector, index) => (
+          <Button className="mx-2" key={connector.uid} variant={index % 2 == 0 ? "outline" : "default"} onClick={() => connect({ connector })} type="button">
             {connector.name}
-          </button>
+          </Button>
         ))}
         <div>{status}</div>
         <div>{error?.message}</div>
+        <Button variant="default">Primary</Button>
+        <Button variant="secondary">Outlined</Button>
+        <Button variant="outline">Outline</Button>
+        <Button variant="ghost">Ghost</Button>
+        <Button variant="outline"> <EnvelopeOpenIcon className="mr-2 h-4 w-4" /> link</Button>
+        <Button disabled>
+          <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+          Please wait
+        </Button>
+
+        <h2>Input</h2>
+        <div className="m-2 p-4 rounded">
+          <div className="m-2 rounded">
+            <Input type="text" placeholder="Reciever Address" />
+          </div>
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label htmlFor="email">Bridge Amount</Label>
+            <Input type="text" id="email" placeholder="Enter amount to bridge" />
+          </div>
+        </div>
       </div>
     </>
   );
