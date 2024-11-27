d5007637f99e9974606a62f823159c753538aef2
Sahil Kashetwar <sahil@enya.ai>
2024-11-27 20:44:02 +0530

    implementation of trade and ecosystem with responsive

--- src/layout/ecosystem/index.tsx
+++ src/layout/ecosystem/index.tsx
@@ -1,10 +1,59 @@
-import { Card, CardContent, Text } from '@/components/ui';
+import { Card, CardContent, Select, SelectContent, SelectItem, SelectTrigger, SelectValue, Text, Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui';
 import { useFetchData } from '@/hooks/useFetchData';
 import { DATA_URL_ECOSYSTEM_LIST, DATA_URL_TRADE_ASSET } from '@/utils/constant';
-import { ChevronRightIcon, ExternalLinkIcon } from '@radix-ui/react-icons';
+import { ExternalLinkIcon } from '@radix-ui/react-icons';
+import { useState } from 'react';
+
+const ecoOption = [
+  {
+    label: "All",
+    value: "All"
+  },
+  {
+    label: "Tools",
+    value: "Tools"
+  },
+  {
+    label: "Oracles",
+    value: "Oracles"
+  },
+  {
+    label: "Decentralized finance",
+    value: "Decentralized finance"
+  },
+  {
+    label: "Centralized exchange",
+    value: "Centralized exchange"
+  },
+  {
+    label: "Onramp",
+    value: "Onramp"
+  },
+  {
+    label: "Bridge",
+    value: "Bridge"
+  },
+  {
+    label: "Wallet",
+    value: "Wallet"
+  },
+  {
+    label: "NFT",
+    value: "NFT"
+  },
+  {
+    label: "Gaming",
+    value: "Gaming"
+  },
+  {
+    label: "DAO",
+    value: "DAO"
+  },
+]
 
 const EcosystemPage = () => {
   const dataUrl = DATA_URL_ECOSYSTEM_LIST;
+  const [selectedFilter, setSelectedFilter] = useState(ecoOption[0].value)
 
   const { isLoading, error, data } = useFetchData(dataUrl);
 
@@ -20,76 +69,62 @@ const EcosystemPage = () => {
           <p className="text-red-500 text-lg font-medium">Error loading data, please try later!</p>
         </div>
       ) : (
-        <div className="flex flex-col gap-5 items-center justify-center p-5 mx-auto w-full md:w-10/12">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 md:gap-8">
-            {data?.map((appItem: any, index: any) => (
-              <Card key={index} className="shadow-lg rounded-xl">
-                <CardContent className="p-6 flex flex-col items-center gap-1">
-                  <img
-                    src={`${DATA_URL_TRADE_ASSET}${appItem.icon.dark}`}
-                    alt={appItem.name}
-                    className="w-16 h-16 rounded-full mb-4"
-                  />
-                  <a
-                    href={appItem.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="hover:underline cursor-pointer"
-                  >
+            <div className="flex flex-col gap-5 items-center justify-center p-5 mx-auto w-full lg:w-10/12">
+              <div className="flex items-start justify-start w-full">
+                <Select onValueChange={(value) => {
+                  setSelectedFilter(value)
+                }} defaultValue={selectedFilter} value={selectedFilter}>
+                  <SelectTrigger className="w-[200px]">
+                    <SelectValue placeholder="Theme" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {ecoOption.map((option) => <SelectItem value={option.value}>{option.label}</SelectItem>)}
+                  </SelectContent>
+                </Select>
+              </div>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 lg:gap-8">
+                {data.filter((c: any) => selectedFilter === 'All'
+                  ? true
+                  : c.type.toLowerCase() === selectedFilter.toLowerCase())?.map((appItem: any, index: any) => (
+                    <Card key={index} className="shadow-lg rounded-xl">
+                      <CardContent className="p-6 flex flex-col items-center gap-1">
+                        <img
+                          src={`${DATA_URL_TRADE_ASSET}${appItem.icon.dark}`}
+                          alt={appItem.name}
+                          className="w-16 h-16 rounded-full mb-4"
+                        />
+                        <a
+                          href={appItem.link}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="hover:underline cursor-pointer"
+                        >
 
-                    <Text variant="md" fontWeight="medium" className="flex items-center gap-2">{appItem.name}
-                      <ExternalLinkIcon className="h-4 w-4 fill-gray-800 dark:fill-dark-gray-50" />
-                    </Text>
-                  </a>
-                  <Text
-                    variant="xs"
-                    fontWeight="light"
-                    className="text-gray-800 dark:text-dark-gray-50 flex items-center"
-                  >
-                    {appItem.description}
-                  </Text>
-                </CardContent>
-              </Card>
-            ))}
+                          <Text variant="md" fontWeight="medium" className="flex items-center gap-2 text-center">{appItem.name}
+                            <ExternalLinkIcon className="h-4 w-4 fill-gray-800 dark:fill-dark-gray-50" />
+                          </Text>
+                        </a>
+                        <Tooltip>
+                          <TooltipContent className="w-64" >
+                            <Text variant="xs">{appItem.description}</Text>
+                          </TooltipContent>
+                          <TooltipTrigger >
+                            <Text
+                              variant="xs"
+                              fontWeight="light"
+                              className="text-gray-800 dark:text-dark-gray-50 overflow-hidden text-ellipsis h-12 line-clamp-3"
+                            >
+                              {appItem.description}
+                            </Text>
+                          </TooltipTrigger>
+                        </Tooltip>
+
+                      </CardContent>
+                    </Card>
+                  ))}
           </div>
         </div>
       )}
-    </div>
-  );
-
-
-  if (isLoading) return <div className="text-center text-gray-500">Loading...</div>;
-  if (error) return <div className="text-center text-red-500">Error loading data</div>;
-
-  return (
-    <div className="container">
-      <div className="flex gap-5 items-center justify-center p-1 md:p-5 mx-auto w-full md:w-10/12">
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-12">
-          {data?.map((exchange: any, index: any) => (
-            <Card key={index} className="shadow-lg rounded-xl">
-              <CardContent className="p-6 flex flex-col items-center gap-1">
-                <img src={`${DATA_URL_TRADE_ASSET}${exchange.icon.light}`} alt={exchange.name}
-                  className="w-16 h-16 rounded-full mb-4"
-                />
-                <Text variant="md" fontWeight="medium">{exchange.name}</Text>
-                <Text variant="xs" fontWeight="light" className="text-gray-800 dark:text-dark-gray-50 flex items-center">
-                  {exchange.pairName}
-                </Text>
-                <a
-                  href={exchange.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="hover:underline cursor-pointer"
-                >
-                  <Text variant="sm" fontWeight="light" className="text-gray-800 dark:text-dark-gray-50 flex items-center">
-                    Visit Site <ChevronRightIcon className="h-4 w-4 fill-gray-800 dark:fill-dark-gray-50" />
-                  </Text>
-                </a>
-              </CardContent>
-            </Card>
-          ))}
-        </div>
-      </div>
     </div>
   );
 }
--- src/layout/trade/index.tsx
+++ src/layout/trade/index.tsx
@@ -20,23 +20,17 @@ const TradePage = () => {
           <p className="text-red-500 text-lg font-medium">Error loading data, please try later!</p>
         </div>
       ) : (
-        <div className="flex flex-col gap-5 items-center justify-center p-5 mx-auto w-full md:w-10/12">
-          {/* <div className="flex items-start justify-start">
-            <Dropdown
-              buttonLabel="Change trade"
-              menuGroups={[tradeOption]}
-            />
-          </div> */}
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 md:gap-8 lg:gap-12">
+            <div className="flex flex-col gap-5 items-center justify-center p-5 mx-auto w-full lg:w-10/12">
+              <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 md:gap-4 lg:gap-12">
             {data?.map((exchange: any, index: any) => (
               <Card key={index} className="shadow-lg rounded-xl">
                 <CardContent className="p-6 flex flex-col items-center gap-1">
                   <img
-                    src={`${DATA_URL_TRADE_ASSET}${exchange.icon.light}`}
+                    src={`${DATA_URL_TRADE_ASSET}${exchange.icon.dark}`}
                     alt={exchange.name}
                     className="w-16 h-16 rounded-full mb-4"
                   />
-                  <Text variant="md" fontWeight="medium">{exchange.name}</Text>
+                  <Text variant="md" fontWeight="medium" className="text-center" >{exchange.name}</Text>
                   <Text
                     variant="xs"
                     fontWeight="light"
@@ -65,42 +59,6 @@ const TradePage = () => {
           </div>
         </div>
       )}
-    </div>
-  );
-
-
-  if (isLoading) return <div className="text-center text-gray-500">Loading...</div>;
-  if (error) return <div className="text-center text-red-500">Error loading data</div>;
-
-  return (
-    <div className="container">
-      <div className="flex gap-5 items-center justify-center p-1 md:p-5 mx-auto w-full md:w-10/12">
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-12">
-          {data?.map((exchange: any, index: any) => (
-            <Card key={index} className="shadow-lg rounded-xl">
-              <CardContent className="p-6 flex flex-col items-center gap-1">
-                <img src={`${DATA_URL_TRADE_ASSET}${exchange.icon.light}`} alt={exchange.name}
-                  className="w-16 h-16 rounded-full mb-4"
-                />
-                <Text variant="md" fontWeight="medium">{exchange.name}</Text>
-                <Text variant="xs" fontWeight="light" className="text-gray-800 dark:text-dark-gray-50 flex items-center">
-                  {exchange.pairName}
-                </Text>
-                <a
-                  href={exchange.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="hover:underline cursor-pointer"
-                >
-                  <Text variant="sm" fontWeight="light" className="text-gray-800 dark:text-dark-gray-50 flex items-center">
-                    Visit Site <ChevronRightIcon className="h-4 w-4 fill-gray-800 dark:fill-dark-gray-50" />
-                  </Text>
-                </a>
-              </CardContent>
-            </Card>
-          ))}
-        </div>
-      </div>
     </div>
   );
 }
